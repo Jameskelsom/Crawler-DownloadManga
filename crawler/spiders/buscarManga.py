9891e6d93a323aa0cb885a5e8b0027ef2a9323ab
James Kelson <kelsomfire@hotmail.com>
2020-01-02 20:08:40 -0300

    error na importacao

--- crawler/spiders/buscarManga.py
+++ crawler/spiders/buscarManga.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import requests
+import os
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-#concurrent 
+# concurrent
 # scrapy crawl buscarManga -s HTTPCACHE_ENABLED=1
+
 
 class BuscarmangaSpider(CrawlSpider):
     name = 'buscarManga'
-    manga =(input('Digite o nome do manga: ')).replace(' ','-').lower()
+    txt = str(input('Digite o nome do manga: '))
+    manga = txt.replace(' ', '-').lower()
     allowed_domains = ['unionleitor.top']
     start_urls = [f'https://unionleitor.top/manga/{manga}']
     base_urls = ['https://unionleitor.top/z']
@@ -16,18 +19,30 @@ class BuscarmangaSpider(CrawlSpider):
     for url in start_urls:
         r = requests.get(url)
         if r.url in base_urls:
-            print('-'*20)
+            print('-'*30)
             print('Manga não encontrado!!!')
-            print('-'*20)
+            print('-'*30)
         else:
+            try:
+                os.mkdir(manga)
+            except:
+                pass
+
             rules = (
                 Rule(
                     LinkExtractor(
-                        restrict_xpaths=('//div/div[1]/a[contains(@href,"leitor/")]')
-                    ),callback='parse_new'
+                        restrict_xpaths=(
+                            '//div/div[1]/a[contains(@href,"leitor/")]')
+                    ), callback='parse_new'
                 ),
             )
 
     def parse_new(self, response):
-        print(response.css('img.img-responsive'))
-        pass
+        pages = []
+        [pages.append(link) for link in response.css('img.img-responsive::attr("src")').getall()]
+        for page in pages:
+            trt = page.lower().find(self.txt)
+            mangaImg = page[trt:].replace('/','-').replace('https:--','')
+            import ipdb; ipdb.set_trace()
+            with open(self.manga +"/" + mangaImg, 'wb') as f:
+                f.write(response.content)
